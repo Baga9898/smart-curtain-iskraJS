0493a7423f06ca1e5aa4d6a398394e2fdde4a419
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-12-28 22:43:01 +0300

    Manual mode is ready

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const ir          = require('ir-receiver').connect(P0);
 const light       = require('led').connect(P1);
 const lightSensor = require('light-sensor').connect(A0);
-const servo       = require('servo').connect(P8).write(0);
+const servo       = require('servo').connect(P8);
 
-const decrementButton = 378124359;
-const incrementButton = 378101919;
-const modeButtonOff   = 378126399;
-const modeButtonOn    = 378077439;
-const servoMaxValue   = 90;
-const servoMinValue   = 0;
-let isAutoMode        = true;
+const bottomButton  = 378124359;
+const topButton     = 378101919;
+const modeButtonOff = 378126399;
+const modeButtonOn  = 378077439;
+const stopButton    = 378091719;
+
+const speedMaxCW  = 544;
+const speedMaxCCW = 2400;
+const speedStop   = (speedMaxCW + speedMaxCCW) / 2;
+
+let curtainState = 'up';
+let isAutoMode   = true;
 
 const setServoValue = (value) => {
-  servo.write(value);
+  servo.write(value, 'us');
 };
 
 ir.on('receive', (code, repeat) => {
@@ -28,10 +33,12 @@ ir.on('receive', (code, repeat) => {
     if (repeat) {
       return;
     } else {
-      if (code === incrementButton) {
-        setServoValue(servoMaxValue);
-      } else if (code === decrementButton) {
-        setServoValue(servoMinValue);
+      if (code === topButton) {
+        setServoValue(speedMaxCCW);
+      } else if (code === bottomButton) {
+        setServoValue(speedMaxCW);
+      } else if (code === stopButton) {
+        setServoValue(speedStop);
       }
     }
   }
@@ -40,9 +47,9 @@ ir.on('receive', (code, repeat) => {
 const handleLuxesCheck = (luxes) => {
   if (isAutoMode) {
     if (luxes > 50) {
-      setServoValue(servoMaxValue);
+      console.log('yep');
     } else {
-      setServoValue(servoMinValue);
+      console.log('yep');
     }
   }
 };
